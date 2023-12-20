5afb8b45146c797c222d4f5b7ee004f4d873280c
Christoph Berganski <christoph.berganski@gmail.com>
2023-12-20 18:46:28 +0100

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     # Short name of the package
     name="cs-gamestate",
     # Current version of the package
-    version="0.0.1",
+    version="0.0.2",
     # Which packages to include and from where
     packages=["cs_gamestate"],
     # Short description of the package
